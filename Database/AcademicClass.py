615d65239d7a9aac9fac0b0f07b6a2bb16aaf4ee
namph1377-sketch <namph1377@ut.edu.vn>
2026-01-20 21:51:36 +0700

    Update AcademicClass.py
    Thử chỉnh sửa

--- Database/AcademicClass.py
+++ Database/AcademicClass.py
@@ -1,4 +1,4 @@
 class AcademicClass:
     # Ở hàm __init__: Nhập vào các thuộc tính của bảng AcademicClass trong data model
-    def __init__(self, ClassID, ):
+    def __init__(self, ClassID, ClassName):
         pass
