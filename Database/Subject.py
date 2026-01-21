19196f2af96bd38616e050be73c30f38e2e50f2e
minhthu2025 <thunguyen.141106@gmail.com>
2026-01-21 23:01:41 +0700

    Subject.py

--- Database/Subject.py
+++ Database/Subject.py
@@ -1,4 +1,5 @@
 class Subject:
-    # Ở hàm __init__: Nhập vào các thuộc tính của bảng Subject trong data model
-    def __init__(self, SubjectID, ):
-        pass
+    def __init__(self, subjectID, subjectName, Credits):
+        self.subjectID = subjectID
+        self.subjectName = subjectName
+        self.Credits = Credits
