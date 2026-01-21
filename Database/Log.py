bb258220e3e94b27db4e17148f1925c89a3536f9
minhthu2025 <thunguyen.141106@gmail.com>
2026-01-21 22:58:32 +0700

    Log.py

--- Database/Log.py
+++ Database/Log.py
@@ -1,7 +1,13 @@
 class Log:
-    # Ở hàm __init__: Nhập vào các thuộc tính của bảng Log trong data model
-    def __init__(self, ):
-        pass
+    def __init__(self, LogID, LogType, LogObject, ChangeAt, userID, OldValue=None):
+        self.LogID = LogID
+        self.LogType = LogType
+        self.LogObject = LogObject
+        self.OldValue = OldValue
+        self.ChangeAt = ChangeAt
+        self.userID = userID
 
     def add_log(self, db):
-        pass
+        sql = "INSERT INTO `Log` (LogID, LogType, LogObject, OldValue, ChangeAt, userID) VALUES (%s,%s,%s,%s,%s,%s)"
+        return db.execute_query(sql, (self.LogID, self.LogType, self.LogObject, self.OldValue, self.ChangeAt, self.userID))
+        
