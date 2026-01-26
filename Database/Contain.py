c85b7c77691fb96dc1d17113b572a65f738b4a57
namph1377-sketch <namph1377@ut.edu.vn>
2026-01-26 18:04:43 +0700

    Update Contain.py

--- Database/Contain.py
+++ Database/Contain.py
@@ -1,34 +1,33 @@
 class Contain:
-    def __init__(self, curriculumID, subjectID):
+    def __init__(self, curriculumID, subjectID, semester):
         self.curriculumID = curriculumID
         self.subjectID = subjectID
+        self.semester = semester
 
     @staticmethod
     def get_all_curriculum_information(db):
         query = """
         SELECT
-          c.curriculumID,
-          cur.semester,
-          s.subjectID,
-          s.subjectName,
-          s.Credits
+            c.curriculumID,
+            c.semester,
+            s.subjectID,
+            s.subjectName,
+            s.Credits
         FROM Contain c
-        JOIN Curriculum cur ON c.curriculumID = cur.curriculumID
         JOIN Subject s ON c.subjectID = s.subjectID
+        ORDER BY c.curriculumID, c.semester
         """
         rows = db.fetch_all(query)
 
         result = []
         for r in rows:
             result.append({
-                "curriculum": {
-                    "curriculumID": r[0],
-                    "semester": r[1],
-                },
+                "curriculumID": r[0],
+                "semester": r[1],
                 "subject": {
                     "subjectID": r[2],
                     "subjectName": r[3],
-                    "Credits": r[4],
+                    "Credits": r[4]
                 }
             })
         return result
